44f7e5a797665aba72356aec2727b0fd6010a147
David Skoda <dskoda1@binghamton.edu>
2016-01-03 23:14:56 +0000

    Linked list implementation and tests complete

--- shared/dataStructures/linkedList.js
+++ shared/dataStructures/linkedList.js
@@ -1,12 +1,99 @@
 function ll () {
+    
     this.head = null,
-            this.tail = null,
-            this.addNode = (val) => {
-                return {
-                    data: val,
-                    next: null
-                }
+    this.tail = null,
+    this.addNode = (val) => {
+        //Check for each case:
+        //1. List is empty
+        //2. List has 1 element
+        //3. List has n elements
+        var node = {
+            data: val,
+            next: null
             }
-}
+        if(this.head === null && this.tail === null){
+            
+            this.head = this.tail = node;
+            
+        }else if(this.head === this.tail){
+            
+            this.head.next = node;
+            this.tail = node;
+        }else{
+            this.tail.next = node;
+            this.tail = node;
+        }
+        
+        return node;
+    }
+    this.containsVal = (val) => {
+        //Traverse the list searching for an occurence of the value
+        var node = this.head;
+        while(node.next !== null){
+            if(node.data === val){
+                return node;
+            }else{
+                node = node.next
+            }
+        }
+        
+        return null;
+    }
+    
+    this.removeNode = (node) => {
+        //1. Node is the only one in the list
+        if(this.head === node && this.tail === node){
+            node.next = null;
+            this.head = null;
+            this.tail = null;
+        //2. Node is the head
+        }else if(node === this.head){
+            this.head = node.next;
+            node.next = null
+        //3. Node is the tail
+        }else if(node === this.tail){
+            //Traverse the list until you find the node before the tail
+            var temp = this.head;
+            while(temp.next !== node){
+                temp = temp.next;
+            }
+            temp.next = null;
+            this.tail = temp;
+            node.next = null;
+        //4. Node is something else
+        }else{
+            var temp = this.head;
+            while(temp.next !== node){
+                temp = temp.next;
+            }
+            temp.next = node.next;
+            node.next = null;
+        }
+    }
+    
+    this.filter = (f) => {
+        //Go through each element in the list
+        //Deleting nodes if they fail
+        var node = this.head;
+        while(node !== null){
 
+            if( ! f(node.data)){
+                var temp = node.next;
+                this.removeNode(node);
+                node = temp;
+            }else{
+                node = node.next;
+            }
+        }
+    }
+    
+    this.map = (f) => {
+        var node = this.head;
+        while(node !== null){
+            var newVal = f(node.data);
+            node.data = newVal;
+            node = node.next;
+        }
+    }
+}
 module.exports = ll;
---
+++ test/dsTests/llTest.js
@@ -0,0 +1,248 @@
+var expect = require('chai').expect;
+
+
+describe('/Shared/dataStructures', () => {
+    describe("linkedList.js", () => {
+        var ds = require('../../shared/dataStructures/index')();
+        var ll = ds.ll;
+        describe("List Constructor: ll()", () => {
+            it("Should return an object with head and tail references", () => {
+                var list = new ll();
+
+                expect(list).to.have.any.keys('head');
+                expect(list).to.have.any.keys("tail");
+
+            });
+
+            describe("addNode(val)", () => {
+                it("Should return a node with keys data=val, next=null", () => {
+                        var list = new ll();
+                        var node = list.addNode(4);
+
+                        expect(node).to.have.any.keys('data');
+                        expect(node.data).to.equal(4);
+                        expect(node).to.have.any.keys("next");
+                        expect(node.next).to.be.a('null');
+                    })
+                    //Empty list
+
+                describe("Adding a node to empty list", () => {
+                        it('Should set head and tail to the node returned', () => {
+
+                            var list = new ll();
+
+                            expect(list.head).to.be.a('null');
+                            expect(list.tail).to.be.a('null');
+
+                            var node = list.addNode(5);
+
+                            expect(list.head).to.deep.equal(node);
+                            expect(list.tail).to.deep.equal(node);
+                        });
+                        it('Should set new node to have null next reference', () => {
+
+                            var list = new ll();
+
+                            var node = list.addNode(5);
+
+                            expect(node.next).to.be.a('null');
+
+                        })
+                    })
+                    //List with 1 element
+                describe('Adding a node to a list with 1 element', () => {
+                    var list = new ll();
+                    list.addNode(5);
+
+                    var node = list.addNode(8);
+
+                    it('Should set head\'s next to the new node', () => {
+
+                        expect(list.head.next).to.deep.equal(node);
+
+                    })
+                    it('Should set tail to the new node', () => {
+
+                        expect(list.tail).to.deep.equal(node);
+                    })
+                });
+                //List with n elements
+                describe('Adding a node to a list with n elements', () => {
+                    var list = new ll();
+                    var head = list.addNode(8);
+                    var oldTail = list.addNode(15);
+                    var newTail = list.addNode(10);
+                    it('Should set old tail\'s next to new node', () => {
+                        expect(oldTail.next).to.deep.equal(newTail);
+                    });
+                    it('Should set list tail to be new node', () => {
+                        expect(list.tail).to.deep.equal(newTail);
+                    })
+                    it('Should set new node to have null next reference', () => {
+
+                        expect(newTail.next).to.be.a('null');
+                    })
+                })
+            })
+
+            //Contains value in list
+            describe('containsVal(val)', () => {
+                var list = new ll();
+                list.addNode(6);
+                list.addNode(8);
+                var foundNode = list.addNode(10);
+                list.addNode(5);
+                list.addNode(15);
+
+                it('Should return the first node with val passed if val is in list', () => {
+                    var node = list.containsVal(10);
+                    expect(node).to.deep.equal(foundNode);
+
+                })
+                it('Should return null if val passed is not in list', () => {
+                    var node = list.containsVal(9);
+                    expect(node).to.be.a('null')
+                })
+            })
+
+            //Delete a node from the list
+            describe('deleteNode(node)', () => {
+
+                describe('Delete the only node in the list', () => {
+                    var list = new ll();
+                    var onlyNode = list.addNode(6);
+
+                    it('Should set head and tail to be null', () => {
+                        list.removeNode(onlyNode);
+
+                        expect(list.head).to.be.a('null');
+                        expect(list.tail).to.be.a('null');
+                    })
+
+                });
+
+                describe('Delete the head of the list', () => {
+
+                    var list = new ll();
+                    var oldHead = list.addNode(6);
+                    list.addNode(8);
+                    var newHead = oldHead.next;
+                    list.removeNode(list.head);
+                    //Or .. list.removeNode(oldHead) works too
+
+                    it('Should set the head of the list to be next of the deleted node', () => {
+                        expect(list.head).to.deep.equal(newHead);
+
+                    });
+                    it('Should null the deleted node\'s next ref', () => {
+                        expect(oldHead.next).to.be.a('null');
+
+                    })
+
+                })
+                describe('Delete the tail of the list', () => {
+
+                    var list = new ll();
+                    list.addNode(5);
+                    list.addNode(17);
+                    var newTail = list.addNode(10);
+                    var oldTail = list.addNode(8);
+                    list.removeNode(list.tail);
+                    //Or.. list.remove(oldTail) works
+
+                    it('Should set the tail to be one node before the old tail', () => {
+                        expect(list.tail).to.deep.equal(newTail);
+                    });
+                    it('Should null the deleted node\'s next ref', () => {
+                        expect(oldTail.next).to.be.a('null');
+
+                    });
+                    it('Should null the new tail\'s next ref', () => {
+                        expect(list.tail.next).to.be.a('null');
+                    });
+                })
+                describe('Delete some middle node in the list', () => {
+                    var list = new ll();
+                    list.addNode(5);
+                    var nodeBefore = list.addNode(17);
+                    var deleteNode = list.addNode(13);
+                    var nodeAfter = list.addNode(18);
+                    list.addNode(6);
+
+                    list.removeNode(deleteNode);
+
+                    it('Should set the node before\'s next to be the node after', () => {
+                        expect(nodeBefore.next).to.deep.equal(nodeAfter);
+                    })
+                    it('Should null the deleted nodes next ref', () => {
+                        expect(deleteNode.next).to.be.a('null');
+                    });
+                    //Not contains?
+                })
+            })
+
+            //Filter list 
+            describe('filterList(func)', () => {
+                var list = new ll();
+                list.addNode(5);
+                list.addNode(17);
+                list.addNode(13);
+                list.addNode(18);
+                list.addNode(6);
+
+                var afterList = new ll();
+                afterList.addNode(17);
+                afterList.addNode(13);
+                afterList.addNode(18);
+                var func = (x) => x > 10;
+                list.filter(func);
+                it("Should take a variable of function type", () => {
+                    expect(func).to.be.instanceof(Function);
+                })
+                it('Should filter a linked list by a comparison function to be called on each node in the list', () => {
+                    var node1 = list.head;
+                    var node2 = afterList.head;
+                    while (node1 !== null && node2 !== null) {
+                        expect(node1).to.deep.equal(node2);
+                        node1 = node1.next;
+                        node2 = node2.next;
+                    }
+                    expect(node1).to.deep.equal(node2);
+                    expect(node1).to.be.a('null');
+                    expect(node2).to.be.a('null');
+                })
+            });
+
+            //Map list
+            describe('mapList(func)', () => {
+                var list = new ll();
+                list.addNode(5);
+                list.addNode(17);
+                list.addNode(6);
+
+                var afterList = new ll()
+                afterList.addNode(50);
+                afterList.addNode(170);
+                afterList.addNode(60);
+
+                var func = (x) => x * 10;
+                list.map(func);
+                it("Should take a variable of function type", () => {
+                    expect(func).to.be.instanceof(Function);
+                })
+                it('Should map a function onto each node of a list', () => {
+                    var node1 = list.head;
+                    var node2 = afterList.head;
+                    while (node1 !== null && node2 !== null) {
+                        expect(node1).to.deep.equal(node2);
+                        node1 = node1.next;
+                        node2 = node2.next;
+                    }
+                    expect(node1).to.deep.equal(node2);
+                    expect(node1).to.be.a('null');
+                    expect(node2).to.be.a('null');
+                })
+            })
+        })
+    })
+})
