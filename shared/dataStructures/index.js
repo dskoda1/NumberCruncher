076c5cca879b9a1ca137e00776e43e188052d186
David Skoda <dskoda1@binghamton.edu>
2016-01-15 10:37:14 -0700

    testing gitignore works

--- shared/dataStructures/index.js
+++ shared/dataStructures/index.js
@@ -2,11 +2,12 @@ module.exports = () => {
     var llConstructor = require('./linkedList');
     var bstConstructor = require('./bst');
     var hashConstructor = require('./hashTable');
+    var x = 0;
     return {
-        
+
         ll: llConstructor,
         bst: bstConstructor,
         hashTable: hashConstructor
-        
+
     }
 }
